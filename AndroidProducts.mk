1550350b4c941ea7b0eb468283cfdbcc46596f98
MASTERGUY <murtipipo@gmail.com>
2020-09-18 04:04:56 +0000

    Yaap BringUp
    Signed-off-by: MASTERGUY <murtipipo@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,4 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_tissot.mk
+    $(LOCAL_DIR)/yaap_tissot.mk
+
+COMMON_LUNCH_CHOICES := \
+    yaap_tissot-user \
+    yaap_tissot-userdebug \
+    yaap_tissot-eng \

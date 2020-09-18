7c9e32eed0244ba7e61bf08ab9c400a464c499e4
MASTERGUY <murtipipo@gmail.com>
2020-09-18 04:04:56 +0000

    Yaap BringUp

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_tissot.mk
+    $(LOCAL_DIR)/yaap_tissot.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_tissot-user \
-    derp_tissot-userdebug \
-    derp_tissot-eng \
+    yaap_tissot-user \
+    yaap_tissot-userdebug \
+    yaap_tissot-eng \

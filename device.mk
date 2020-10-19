276def7fae0c9e3cb1782bd730eeb9225ee9bee8
MASTERGUY <murtipipo@gmail.com>
2020-10-19 06:35:02 +0000

    tisot: build messaging

--- device.mk
+++ device.mk
@@ -78,6 +78,10 @@ PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
     android.hardware.ir@1.0-service
 
+# Messaging
+PRODUCT_PACKAGES += \
+   messaging
+
 # Properties
 -include device/xiaomi/tissot/prop.mk
 
